5179dbbe437bf6f4fe1c3a58eb569bffd4a134d3
Alexander Abramov <richesantiques9@gmail.com>
2025-10-30 01:56:50 +0400

    PostgreSQL is connected, a database is created, migrations are performed, and the admin panel is configure

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')  # id и name в списке
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')  # id, name, price, category
+    list_filter = ('category',)  # фильтрация по категории
+    search_fields = ('name', 'description')  # поиск по name и description
