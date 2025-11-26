062e11af6f013008d5e29ad4d4d44518bdfcf697
Alexander Abramov <richesantiques9@gmail.com>
2025-11-27 00:44:41 +0400

    protected product management controllers using LoginRequiredMixin

--- config/settings.py
+++ config/settings.py
@@ -119,6 +119,7 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Media files (uploads)
@@ -147,3 +148,8 @@ EMAIL_TIMEOUT = 10  # Таймаут для почтовых операций
 
 # Дополнительные настройки для корректной работы
 EMAIL_SUBJECT_PREFIX = '[Skystore] '  # Префикс для всех писем
+
+# Настройки аутентификации
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
